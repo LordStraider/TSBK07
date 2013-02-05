3db1ad3286a43a3b26453e83875bb837444f4987
jonathan karlsson <jonathan.s.karlsson@gmail.com>
2013-02-05 11:14:14 +0100

    a nice windmill

--- lab3/lab3-1.c
+++ lab3/lab3-1.c
@@ -1,17 +1,6 @@
-/* Lab 1-1.
-// This is the same as the first simple example in the course book,
-// but with a few error checks.
-// Remember to copy your file to a new on appropriate places during the lab so you keep old results.
-// Note that the files "lab1-1.frag", "lab1-1.vert" are required.
-
-// Includes vary a bit with platforms.
-// MS Windows needs GLEW or glee.
-// Mac uses slightly different paths.*/
 #ifdef __APPLE__
 	#include <OpenGL/gl3.h>
 	#include "MicroGlut.h"
-	// linking hint for Lightweight IDE
-	//uses framework Cocoa
 #endif
 #include "GL_utilities.h"
 #include "LoadTGA.h"
@@ -22,7 +11,7 @@
 /* Globals*/
 #define PI 3.14159
 
-GLfloat rot[16], trans[16], total[16], cam[16];
+GLfloat rot[16], trans[16], shear[16], total[16], cam[16];
 
 
 #define near 1.0
@@ -36,22 +25,21 @@ GLfloat projMatrix[] = {        2.0f*near/(right-left), 0.0f, (right+left)/(righ
 		                        0.0f, 0.0f, -(far + near)/(far - near), -2*far*near/(far - near),
 		                        0.0f, 0.0f, -1.0f, 0.0f };
 
-unsigned int bunnyVertexArrayObjID;
 Model *bunny;
 Model *cow;
+Model *blade;
+Model *windmillWalls;
+Model *windmillRoof;
+Model *windmillBalcony;
 GLuint program;
 GLuint myTex;
+Point3D p,l;
 
 
 
 
 
 void init(void) {
-	/* two vertex buffer objects, used for uploading the*/
-	unsigned int bunnyTexCoordBufferObjID;
-
-	/* Reference to shader program*/
-
 	/* GL inits*/
 	glClearColor(0.2,0.2,0.5,0);
 	glEnable(GL_DEPTH_TEST);
@@ -61,45 +49,24 @@ void init(void) {
 	printError("GL inits");
 
 	/* Load and compile shader*/
-	program = loadShaders("lab2-1.vert", "lab2-1.frag");
+	program = loadShaders("lab3-1.vert", "lab3-1.frag");
 	glUseProgram(program);
 	printError("init shader");
 	/* Upload geometry to the GPU:*/
 
 	bunny = LoadModelPlus("bunnyplus.obj");
     cow = LoadModelPlus("cow.obj");
+    windmillBalcony = LoadModelPlus("windmill-balcony.obj");
+    windmillRoof = LoadModelPlus("windmill-roof.obj");
+    windmillWalls = LoadModelPlus("windmill-walls.obj");
+    blade = LoadModelPlus("blade.obj");
 
 	LoadTGATextureSimple("maskros512.tga", &myTex);
-
-    glGenBuffers(1, &bunnyTexCoordBufferObjID);
-
-    glBindVertexArray(bunnyVertexArrayObjID);
 
 	glBindTexture(GL_TEXTURE_2D, myTex);
 	glUniform1i(glGetUniformLocation(program, "texUnit"), 0); // Texture unit 0
 
-
-    if (bunny->texCoordArray != NULL)
-    {
-	    glBindBuffer(GL_ARRAY_BUFFER, bunnyTexCoordBufferObjID);
-	    glBufferData(GL_ARRAY_BUFFER, bunny->numVertices*2*sizeof(GLfloat), bunny->texCoordArray, GL_STATIC_DRAW);
-	    glVertexAttribPointer(glGetAttribLocation(program, "inTexCoord"), 2, GL_FLOAT, GL_FALSE, 0, 0);
-	    glEnableVertexAttribArray(glGetAttribLocation(program, "inTexCoord"));
-	}
-
  	/* End of upload of geometry*/
- 	Point3D p,l;
- 	SetVector(0.0, 0.0, 1.0, &p);
- 	SetVector(0.0, 0.0, -3.0, &l);
-
-	lookAt(&p, &l, 0.0, 1.0, 0.0, &cam);
-
-	T(0, 0, -2, trans);
-	Ry(0.0, rot);
-    Mult(rot, trans, total);
-
-	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);
-	glUniformMatrix4fv(glGetUniformLocation(program, "camMatrix"), 1, GL_TRUE, cam);
 	glUniformMatrix4fv(glGetUniformLocation(program, "projMatrix"), 1, GL_TRUE, projMatrix);
 
 
@@ -113,8 +80,15 @@ void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	GLfloat t = (GLfloat)glutGet(GLUT_ELAPSED_TIME);
+ 	SetVector(20.0, 0.0, 0.0, &p);
+ 	SetVector(0.0, 0.0, -2.0, &l);
 
-	T(0, 0, -2, trans);
+	lookAt(&p, &l, 0.0, 1.0, 0.0, &cam);
+
+
+	glUniformMatrix4fv(glGetUniformLocation(program, "camMatrix"), 1, GL_TRUE, cam);
+/*
+	T(0, 0, -6, trans);
 	Ry(t/1000, rot);
     Mult(trans, rot, total);
 	Rx(t/1000, rot);
@@ -123,6 +97,54 @@ void display(void) {
 
     DrawModel(bunny, program, "inPosition", "inNormal", "inTexCoord");
     DrawModel(cow, program, "inPosition", "inNormal", "inTexCoord");
+*/
+/*	T(0, 0, -2, trans);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, trans);
+    DrawModel(blade, program, "inPosition", "inNormal", "inTexCoord");
+	T(0, 1, -2, trans);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, trans);
+    DrawModel(windmillWalls, program, "inPosition", "inNormal", "inTexCoord");
+	T(0, 2, -2, trans);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, trans);
+    DrawModel(windmillRoof, program, "inPosition", "inNormal", "inTexCoord");
+	T(1, 0, -2, trans);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, trans);
+    DrawModel(windmillBalcony, program, "inPosition", "inNormal", "inTexCoord");
+*/
+    T(-5, -7, 0, trans);
+    S(0.8, 0.8, 0.8, shear);
+    Mult(trans, shear, total);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);    DrawModel(windmillWalls, program, "inPosition", "inNormal", "inTexCoord");
+
+    T(0, 0, 0, trans);
+    S(0.5, 0.5, 0.5, shear);
+    Mult(trans, shear, total);
+	Rx(t/1000, rot);
+    Mult(rot, total, total);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);
+    DrawModel(blade, program, "inPosition", "inNormal", "inTexCoord");
+	T(0, 0, 0, trans);
+    S(0.5, 0.5, 0.5, shear);
+    Mult(trans, shear, total);
+	Rx(PI / 2 + t/1000, rot);
+    Mult(rot, total, total);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);
+    DrawModel(blade, program, "inPosition", "inNormal", "inTexCoord");
+	T(0, 0, 0, trans);
+    S(0.5, 0.5, 0.5, shear);
+    Mult(trans, shear, total);
+	Rx(PI + t/1000, rot);
+    Mult(rot, total, total);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);
+    DrawModel(blade, program, "inPosition", "inNormal", "inTexCoord");
+	T(0, 0, 0, trans);
+    S(0.5, 0.5, 0.5, shear);
+    Mult(trans, shear, total);
+	Rx(3 * PI / 2 + t/1000, rot);
+    Mult(rot, total, total);
+	glUniformMatrix4fv(glGetUniformLocation(program, "mdlMatrix"), 1, GL_TRUE, total);
+    DrawModel(blade, program, "inPosition", "inNormal", "inTexCoord");
+
 
 	printError("display");
 
